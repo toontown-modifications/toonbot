2fa83072372845b3c435d5331e6f6453f4f76ee6
Thunderzapper <justinabossmlg@gmail.com>
2021-06-27 20:42:28 -0400

    New stuff

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import discord
+from discord.ext import commands
+from aiohttp import request
+bot = commands.Bot(command_prefix='toon ')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+@bot.command()
+async def ttrdistricts(ctx):
+    API = "https://www.toontownrewritten.com/api/population"
+    async with request("GET", API, headers={}) as response:
+        data = await response.json()
+        await ctx.send(data["populationByDistrict"])
+        print("did ttrdistricts command successfully")
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
-
-client = MyClient()
-client.run('ODU4NDA0NzU2MTY1NDkyNzQ2.YNdpug.DSIPgOe_ljlI4tr_2ttdX9OEvXk')
+bot.run("ODU4NDA0NzU2MTY1NDkyNzQ2.YNdpug.DSIPgOe_ljlI4tr_2ttdX9OEvXk")
